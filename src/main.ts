6bdf6af5045d61202aba910d564f73348450feef
Manuel Romero <manuelromeromoreno13@gmail.com>
2025-11-19 16:45:05 -0500

    back-lambda

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { Express } from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(
+  expressApp: Express | undefined = undefined,
+  port: number | undefined = undefined,
+)
+
+{
+  const app = await NestFactory.create(AppModule, new ExpressAdapter (expressApp));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  if (port) {
+
+    await app.listen(port);
+  }
+  return app;
 }
 bootstrap();
+export async function createApp(expressApp: Express) {
+  return bootstrap(expressApp);
+}
+
