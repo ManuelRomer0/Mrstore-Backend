3b2e5fb91a1f4ba7068974de653ea7e7d4fe8d26
Jesús Sierra <jesussierra615@gmail.com>
2025-11-21 02:00:22 -0500

    serverless.yml fix v4

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { exec } from 'child_process';
+import { error } from 'console';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  onModuleInit() {
+    exec('npm run prisma:migrate', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing migration: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Migration stderr: ${stderr}`);
+        return;
+      }
+      console.log(`Migration stdout: ${stdout}`);
+    });
   }
 }
