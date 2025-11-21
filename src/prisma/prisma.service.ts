3c05c54699961860fb4410ac27ffe5983c01bb4e
Jesús Sierra <jesussierra615@gmail.com>
2025-11-21 02:15:25 -0500

    database connection fix v2

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,31 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { exec } from 'child_process';
+import { env } from 'prisma/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg(
-      {
-        url: process.env.DATABASE_URL,
-      },
-      {
-        onConnectionError: (error) => {
-          console.error('Database connection error:', error);
-          exec('npm run prisma:migrate', (error, stdout, stderr) => {
-            if (error) {
-              console.error(`Error executing migration: ${error.message}`);
-              return;
-            }
-            if (stderr) {
-              console.error(`Migration stderr: ${stderr}`);
-              return;
-            }
-            console.log(`Migration stdout: ${stdout}`);
-          });
-        },
-      },
-    );
+    const adapter = new PrismaPg({
+      url: env('DATABASE_URL'),
+    });
     super({ adapter });
   }
 }
