183e4ae549215847107d332e2a44fa3be55798de
Jesús Sierra <jesussierra615@gmail.com>
2025-11-21 02:20:59 -0500

    database connection fix v3

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { exec } from 'child_process';
-import { env } from 'prisma/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({
-      url: env('DATABASE_URL'),
+      url: process.env.DATABASEURL,
     });
     super({ adapter });
   }
